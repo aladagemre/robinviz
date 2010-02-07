ebfd9eb2b687aada88e831c4c75f9f5e29e161d2
melihsozdinler <melihsozdinler@277e9272-0ffb-11df-962a-0b01395cd2b2>
2010-02-07 08:37:19 +0000

    Orthogonal changed into rectangular since it is not actually orthogonal.

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@ g++ -I$LEDAROOT/incl -L$LEDAROOT src/cpp/tcircular.cpp -lX11 -lm -lL -lG -lP -lW
 g++ -I$LEDAROOT/incl -L$LEDAROOT src/cpp/layered.cpp -lX11 -lm -lL -lG -lP -lW -lD3 -lGeoW -o layered.exe
 g++ -I$LEDAROOT/incl -L$LEDAROOT src/cpp/force.cpp -lX11 -lm -lL -lG -lP -lW -lD3 -lGeoW -o force.exe 
 g++ -I$LEDAROOT/incl -L$LEDAROOT src/cpp/star.cpp -lX11 -lm -lL -lG -lP -lW -lD3 -lGeoW -o star.exe 
-g++ -I$LEDAROOT/incl -L$LEDAROOT src/cpp/orthogonal.cpp -lX11 -lm -lL -lG -lP -lW -lD3 -lGeoW -o orthogonal.exe 
+g++ -I$LEDAROOT/incl -L$LEDAROOT src/cpp/rectangular.cpp -lX11 -lm -lL -lG -lP -lW -lD3 -lGeoW -o rectangular.exe 
 g++ -I$LEDAROOT/incl -L$LEDAROOT src/cpp/simplify.cpp -lX11 -lm -lL -lG -lP -lW -lD3 -lGeoW -o simplify.exe 
 g++ src/cpp/robinviz.cpp -o robinviz.exe 
