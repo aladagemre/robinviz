e043035e6b0bc1214e6172f3bed416c83bbd573f
aladagemre <aladagemre@277e9272-0ffb-11df-962a-0b01395cd2b2>
2010-08-12 00:46:10 +0000

    * Now using sqlite3 for storing GO data. * Can dynamically load data into tree.

--- src/python/robinviz/genetree/xmlparser.py
+++ src/python/robinviz/genetree/xmlparser.py
@@ -1,28 +1,38 @@
 # -*- coding: utf-8 -*-
 from xml.dom.minidom import parse
 import shelve
+import sqlite3
+import os
 
+	
+def createTable(curs):
+    try:
+	curs.execute('''
+	CREATE TABLE terms (
+	    id		INT	PRIMARY KEY,
+	    name	TEXT,
+	    children	TEXT
+	) '''
+	)
+    except sqlite3.OperationalError:
+	print "Table already exists"
+	
 
-class Term:
-    def __init__(self, accession=None):
-	self.accession = accession
-	self.name = None
-	self.children = []
-	
-    def add_child(self, term):
-	self.children.append(term)
-	
-    def __repr__(self):
-	return "%s (%s) - %s" % (self.accession, self.name, self.children)
-	
 def generateTermDict():
     dom1 = parse("go_daily-termdb.rdf-xml/go_daily-termdb.rdf-xml")
     #dom1 = parse("go_daily-termdb.rdf-xml/slim.xml")
     terms = dom1.childNodes[1].childNodes[1].childNodes
+    
+    try:
+	os.remove("sqlite.db")
+    except:
+	pass
+    
+    conn = sqlite3.connect("sqlite.db")
+    curs = conn.cursor()
+    createTable(curs)
 
-
-    term_dict = shelve.open("termdict.db")
-
+    children_dict = {}
     for i in range(len(terms)):
 	# For all term block, find the subnodes:
 	cn = terms[i].childNodes
@@ -32,75 +42,49 @@ def generateTermDict():
 		# for each subnode tag, look its name
 		if "accession" in element.nodeName:
 		    # if we see accession, it means it's the name of the term.
-		    term = Term( str(element.childNodes[0].nodeValue) )
-		    term_dict[term.accession] = term
-		    
+		    term_accession = str(element.childNodes[0].nodeValue)
+		    try:
+			term_id = int(term_accession.split("GO:")[1])
+		    except IndexError:
+			print "Index error:", term_accession
+			continue
 		elif  "is_a" in element.nodeName:
 		    is_a_info =element.getAttribute("rdf:resource")
 		    if "GO:" in is_a_info:
-			parent_accession = str("GO:"+ is_a_info.split("GO:")[1])
+			parent_id = int(is_a_info.split("GO:")[1])
+			#parent_accession = str("GO:"+ term_id)
 			
 			try:
-			    parent = term_dict[parent_accession]
-			except KeyError:
-			    new_term = Term( parent_accession )
-			    parent = new_term
-			    #raise Exception("No such parent has ever created! Parent: %s, Child: %s" % ( parent_accession, term.accession ) ) # this shouldnt happen!
-			#print "adding child %s to %s" % (term, parent)
-			#print "Before:", parent
-			parent.add_child(term)
-			#print "After:", parent
-			term_dict[parent_accession] = parent
-			    
-    #print term_dict
-    return term_dict
-
-def getTermDict():
-    return shelve.open("termdict.db")
+			    child_list = children_dict[parent_id]
+			except:
+			    child_list = []
+			child_list.append(str(term_id))
+			children_dict[parent_id] = child_list
+			
+		elif "name" in element.nodeName:
+		    name = str(element.childNodes[0].nodeValue)
+		    #curs.execute("UPDATE terms SET name = '%s' where id = %d" % (name, int(term.accession.split("GO:")[1])) )
+		    #curs.execute("UPDATE terms SET children = children || ' %s' where RowID = %d" % (term.accession,term_id) )
+	    if name == "all":
+		continue
+	    fields =( str(term_id), '"%s"' % name, '""' )
+	    statement = 'INSERT INTO terms VALUES(%s);' % ",".join( fields )
+	    #print "Executing:", statement
+	    try:
+		curs.execute( statement )
+	    except:
+		print statement
+		print 
+    for parent_id, child_list in children_dict.iteritems():
+	statement = "UPDATE terms SET children = '%s' where id = %d;" % (",".join(sorted(child_list)), parent_id) 
+	
+	try:
+	    curs.execute( statement )
+	except:
+	    print statement
     
+    conn.commit()
+    conn.close()
+
 if __name__ == "__main__":
-    d = generateTermDict()
-    d.close()
-    """
-    t1 = Term("GO:0000001")
-    t1.name="Term1"
-    t2 = Term("GO:0000002")
-    t2.name="Term2"
-    t3 = Term("GO:0000003")
-    t3.name="Term3"
-    t4 = Term("GO:0000004")
-    t4.name="Term4"
-    
-    t1.add_child(t2)
-    t1.add_child(t3)
-    t3.add_child(t4)
-    
-    print t1"""
-
-
-
-"""
-def traverse(node, level):
-    print "\t" * level + node
-    children = term_dict[node]
-    for child in children:
-	traverse(child, level+1)
-    
-for parent in ("GO:0008150", "GO:0005575", "GO:0003674"):
-    traverse(parent, 0)
-output = ""
-"""
-		    
-"""
-for i in range(1, len(terms), 2):
-    #print terms[i].childNodes[1].childNodes[0].nodeValue
-    for x in terms[i].childNodes:
-	if x.childNodes:
-	    
-	    value=  x.childNodes[0].nodeValue.strip()
-	    if value:
-		print "'%s'" % value
-    
-""" 
-    
-#terms[5].childNodes[1].childNodes[0].nodeValue
+    generateTermDict()
