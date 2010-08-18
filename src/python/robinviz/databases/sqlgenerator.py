a5139b18e271bc7ec564f77ab37729a9961718a1
aladagemre <aladagemre@277e9272-0ffb-11df-962a-0b01395cd2b2>
2010-08-18 22:56:35 +0000

    * Added auto download/unzip feautre. * Added indexing

--- src/python/robinviz/databases/sqlgenerator.py
+++ src/python/robinviz/databases/sqlgenerator.py
@@ -4,8 +4,40 @@ Generates a sqlite database from the BIOGRID-IDENTIFIERS.tab.txt file.
 http://thebiogrid.org/downloads/archives/Release%20Archive/BIOGRID-3.0.67/BIOGRID-IDENTIFIERS-3.0.67.tab.zip
 """
 import sqlite3
+import urllib
+import os
+import zipfile
 
-conn = sqlite3.connect("identifier2.db")
+
+# ==================================
+# ============ METHODS =============
+def download_file(url):
+    webFile = urllib.urlopen(url)
+    localFile = open(url.split('/')[-1], 'w')
+    localFile.write(webFile.read())
+    webFile.close()
+    localFile.close()
+
+# ===================================
+# ============ PARAMETERS ===========
+version = "3.0.67"
+zipname = "BIOGRID-IDENTIFIERS-%s.tab.zip" % version
+filename = "BIOGRID-IDENTIFIERS-%s.tab.txt" % version
+fileurl = "http://thebiogrid.org/downloads/archives/Release Archive/BIOGRID-%s/%s" % (version, zipname)
+
+
+# =======================================
+# ============ INITIALIZATION ===========
+if not os.path.exists(filename):
+    print "txt file not found, zip file downloading..."
+    download_file(fileurl)
+    print "zip file downloaded. Now extracting..."
+    zf = zipfile.ZipFile(zipname)
+    zf.extractall()
+    print "Extracted."
+
+print "SQL conversion starts..."
+conn = sqlite3.connect("identifier.db")
 curs = conn.cursor()
 curs.execute("DROP TABLE IF EXISTS translation;")
 curs.execute("""
@@ -14,10 +46,12 @@ CREATE TABLE "translation" (
     "identifier_value" TEXT NOT NULL,
     "identifier_type" TEXT NOT NULL
 )
-
-
 """)
-input_stream = open("BIOGRID-IDENTIFIERS-3.0.67.tab.txt")
+
+curs.execute('CREATE INDEX "biogrid_index" on translation (biogrid_id ASC);')
+curs.execute('CREATE UNIQUE INDEX "identifier_index" on translation (identifier_value ASC)')
+
+input_stream = open(filename)
 #output_stream = open("sqlstatements.txt","w")
 
 for line in input_stream: 
