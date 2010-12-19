86d331ab99cb555439b01c7c845620ace2315ae9
aladagemre <aladagemre@277e9272-0ffb-11df-962a-0b01395cd2b2>
2010-12-19 12:50:46 +0000

    Preconf generator: geodata won't be copied in Co-Ontology mode

--- preconf-generator.py
+++ preconf-generator.py
@@ -23,6 +23,7 @@ def prepare():
 
     shutil.copy('settings.yaml', 'temp/settings.yaml')
     shutil.copy('outputs/resultparams.txt', 'temp/outputs/resultparams.txt')
+    confirmation_type = open("outputs/resultparams.txt").read().strip()
 
     assocdata_files = ('go_slim.txt', 'selected_assoc.txt', 'input_go.txt', 'category_codes.txt',)
     godata_files = ('selected_terms.txt',)
@@ -43,11 +44,12 @@ def prepare():
 	source = 'src/python/robinviz/databases/inputtrees/ppidata/%s' % filename
 	target = 'temp/%s' % source
 	shutil.copy(source, target)
-	
-    for filename in geodata_files:
-	source = 'src/python/robinviz/databases/inputtrees/geodata/%s' % filename
-	target = 'temp/%s' % source
-	shutil.copy(source, target)
+
+    if confirmation_type.startswith( "co_expression.exe" ):
+	for filename in geodata_files:
+	    source = 'src/python/robinviz/databases/inputtrees/geodata/%s' % filename
+	    target = 'temp/%s' % source
+	    shutil.copy(source, target)
     
 
 
@@ -59,7 +61,7 @@ if __name__ == '__main__':
 	compress_files( sys.argv[1], ['src', 'outputs','settings.yaml'] )
 	shutil.move(sys.argv[1], "../sources/preconfigurations/%s" % sys.argv[1] )
 	os.chdir("..")
-	#clean()
+	clean()
 	
     else:
 	print "Usage: python preconf-generator.py PreConfigName.tar.gz"
